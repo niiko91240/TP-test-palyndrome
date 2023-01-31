afaeefb36c08e570b6fa72bbc40e824c08b930f6
niiko fx <nicolas.ajrd@gmail.com>
2023-01-31 19:54:11 +0100

    Etape 4 - Test moment de la journée

--- test/etape4/test_ohce.py
+++ test/etape4/test_ohce.py
@@ -2,18 +2,29 @@ import unittest
 from src.etape4.Ohce import Ohce
 from src.etape4.Langues.LangueFrancaise import LangueFrancaise
 from src.etape4.Langues.LangueAnglaise import LangueAnglaise
-from datetime import datetime
+import datetime
 import locale
 
 
 class TestMain(unittest.TestCase):
 
+    langue_systeme = locale.getdefaultlocale()[0]
+
     def test_langue_systeme(self):
-        langue_systeme = locale.getdefaultlocale()[0]
-        if(langue_systeme == 'en_GB'):
-            self.assertEqual(Ohce(langue_systeme).bien_dit(), 'Well done !')
+
+        if(self.langue_systeme == 'en_GB'):
+            self.assertEqual(Ohce(self.langue_systeme).bien_dit(), 'Well done !')
         else:
-            self.assertEqual(Ohce(langue_systeme).bien_dit(), 'Bien dit !')
+            self.assertEqual(Ohce(self.langue_systeme).bien_dit(), 'Bien dit !')
+
+    def test_heure_systeme(self):
+        now = datetime.datetime.now()
+        if(now.hour < 11):
+            self.assertEqual(Ohce(self.langue_systeme).periode(now), 'MATIN')
+        elif(now.hour < 18):
+            self.assertEqual(Ohce(self.langue_systeme).periode(now), 'JOURNEE')
+        else:
+            self.assertEqual(Ohce(self.langue_systeme).periode(now), 'SOIR')
 
 
 
