fdad004f2e3efd67a622b8ab1d11dd8ba02bd18d
niiko fx <nicolas.ajrd@gmail.com>
2023-01-31 18:42:52 +0100

    Etape 2 - Bonjour avec test

--- test/etape2/test_ohce.py
+++ test/etape2/test_ohce.py
@@ -14,7 +14,29 @@ class TestMain(unittest.TestCase):
     def test_bien_dit_en(self):
         self.assertEqual(Ohce(LangueAnglaise).bien_dit(), 'Well done !')
 
+    def test_hello_morning_fr(self):
+        date = datetime.now().replace(hour=10)
+        self.assertEqual(Ohce(LangueFrancaise).bonjour(date), 'Allez on se réveille !')
 
+    def test_hello_morning_en(self):
+        date = datetime.now().replace(hour=10)
+        self.assertEqual(Ohce(LangueAnglaise).bonjour(date), 'Wake up !')
+
+    def test_hello_moon_fr(self):
+        date = datetime.now().replace(hour=12)
+        self.assertEqual(Ohce(LangueFrancaise).bonjour(date), 'Bonjour !')
+
+    def test_hello_moon_en(self):
+        date = datetime.now().replace(hour=12)
+        self.assertEqual(Ohce(LangueAnglaise).bonjour(date), 'Hello !')
+
+    def test_hello_aftermoon_fr(self):
+        date = datetime.now().replace(hour=19)
+        self.assertEqual(Ohce(LangueFrancaise).bonjour(date), 'Bonsoir !')
+
+    def test_hello_aftermoon_en(self):
+        date = datetime.now().replace(hour=19)
+        self.assertEqual(Ohce(LangueAnglaise).bonjour(date), 'Good Evening !')
 
 
 
